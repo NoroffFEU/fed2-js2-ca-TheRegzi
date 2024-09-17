95682d8a7b7cd259fbfc445009ca29fda73de494
Regine Kornbakk <142594277+TheRegzi@users.noreply.github.com>
2024-09-17 15:05:26 +0200

    Added hamburger menu for all html pages

--- src/app.js
+++ src/app.js
@@ -3,3 +3,14 @@ import "./css/style.css";
 import router from "./js/router";
 
 await router(window.location.pathname);
+
+document.getElementById('hamburger-button').addEventListener('click', function() {
+    var content = document.getElementById('hamburger-content');
+    if (content.classList.contains('hidden')) {
+        content.classList.remove('hidden');
+        content.classList.add('visible');
+    } else {
+        content.classList.remove('visible');
+        content.classList.add('hidden');
+    }
+});
