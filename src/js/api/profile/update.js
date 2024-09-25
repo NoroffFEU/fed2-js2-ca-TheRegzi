4be10abd47df2d756b4a90a013e8b31025c19402
Regine Kornbakk <142594277+TheRegzi@users.noreply.github.com>
2024-09-25 16:04:06 +0200

    feat: improved code, added JS docs

--- src/js/api/profile/update.js
+++ src/js/api/profile/update.js
@@ -2,6 +2,15 @@ import { headers } from "../headers";
 import { API_SOCIAL_PROFILES } from "../constants";
 import { fetchProfile } from "./read";
 
+/**
+ * This function fetches the logged-in user's profile using the stored username from `localStorage`, 
+ * and populates the 'update' form fields with the user's bio and avatar URL.
+ * Logs an error to the console if it fails to fetch the profile data.
+ * 
+ * @returns {void}
+ * @throws {Error} Throws an error if it fails to fetch the profile data.
+ */
+
 async function populateForm() {
     try {
         const username = localStorage.getItem('name'); 
@@ -16,10 +25,20 @@ async function populateForm() {
 
 populateForm();
 
-export async function updateProfile(username, { avatar }) {
+/**
+ * Updates the profile data of the logged in user by sending a 'PUT' request to the API.
+ * It updates the profile's bio and avatar by sending new data to the API.
+ * If the update request fails, an error is thrown with a message from the API or a default message.
+ * 
+ * @param {string} username The username of the logged-in user whose profile is being updated.
+ * @param {string} avatar The URL of the user's avatar.
+ * @returns {Promise<object>} A promise that resolves to the updated profile data in JSON format.
+ * @throws {Error} Throws an error message from the API or a default message if the update fails or response is not okay. 
+ */
+
+export async function updateProfile(username, { bio, avatar }) {
 
     const apiUrl = `${API_SOCIAL_PROFILES}/${username}`;
-    const bio = document.getElementById('update-bio').value;
 
     try {
         const requestHeaders = await headers();
