1e68f3e6ec8bab58e2cf086af3d6561c8f655e48
Regine Kornbakk <142594277+TheRegzi@users.noreply.github.com>
2024-09-25 16:51:50 +0200

    feat: added JS docs to updateProfile.js

--- src/js/router/views/updateProfile.js
+++ src/js/router/views/updateProfile.js
@@ -3,6 +3,15 @@ import { updateProfile } from "../../api/profile/update"
 
 authGuard();
 
+/**
+ * Handles the profile update form submission by gathering bio and avatar URL values,
+ * and sending them to the updateProfile function.
+ * If the profile is updated successfully, the user is redirected to the profile page.
+ * If an error occurs, it logs the error to the console.
+ *
+ * @returns {void} This function doesn't return any value.
+ */
+
 const updateProfileForm = document.getElementById('updateProfile');
 
 if (updateProfileForm) {
