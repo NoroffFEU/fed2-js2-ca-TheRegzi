01edf56e8a21ca96f7d4b6f50b516a1f0d7d371d
Regine Kornbakk <142594277+TheRegzi@users.noreply.github.com>
2024-09-25 14:09:05 +0200

    feat: added JS docs for create.js, removed console logs

--- src/js/api/post/create.js
+++ src/js/api/post/create.js
@@ -1,6 +1,20 @@
 import { headers } from "../headers";
 import { API_SOCIAL_POSTS } from "../constants";
 
+/**
+ * Creates a new social media post by sending a POST request to the "API_SOCIAL_POSTS" endpoint.
+ * The 'formData' should contain the fields title, body, media and tags. 
+ * The `tags` field is expected to be a comma-separated string, which is split into an array of tags. 
+ * The `media` field is optional and represents an image with a URL and alt text.
+ * If the creation of the post fails, it throws an error message from the API or a default message.
+ * 
+ * @async
+ * @param {FormData} formData The content inside the form. For example: 'formData.get('title')'.
+ * @returns {Promise<object>} A promise that resolves to the response data in JSON format.
+ * @throws {Error} Throws an error if the post creation fails or if the response is not okay.
+ * 
+ */
+
 export async function createPost(formData) {
     
     const apiUrl = API_SOCIAL_POSTS;
@@ -24,13 +38,10 @@ export async function createPost(formData) {
 
         if (!response.ok) {
             const errorData = await response.json();
-            console.log('Response status:', response.status);
-            console.log('Error data:', errorData);
             throw new Error(`Failed to create post: ${errorData.message || response.status}`);
         }
 
         const data = await response.json();
-        console.log('Success:', data);
         return data;
 
     } catch (error) {
