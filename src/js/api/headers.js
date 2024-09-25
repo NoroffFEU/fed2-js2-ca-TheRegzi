484c4e301eca44f86448a1485167ea0d61c70eb5
Regine Kornbakk <142594277+TheRegzi@users.noreply.github.com>
2024-09-25 16:11:38 +0200

    feat: added JS docs to headers.js

--- src/js/api/headers.js
+++ src/js/api/headers.js
@@ -1,5 +1,18 @@
 import { getKey } from "./auth/key";
 
+/**
+ * Constructs and returns the headers needed for API requests.
+ * 
+ * This function sets the `'Content-Type': 'application/json'` header for all requests.
+ * If available, it also appends the API key and the user's authentication token from localStorage.
+ * 
+ * - The API key (`X-Noroff-API-Key`) is retrieved via `getKey('My API Key Name')`.
+ * - The Authorization token (`Bearer token`) is retrieved from localStorage.
+ * 
+ * @returns {Headers} An instance of the Headers object to be used in API requests.
+ * 
+ */
+
 export async function headers() {
   const headers = new Headers();
  
