17bc7eee26be4a95a9cf8a78e518d04515245999
Regine Kornbakk <142594277+TheRegzi@users.noreply.github.com>
2024-09-25 14:22:11 +0200

    feat: added JS docs to delete.js

--- src/js/api/post/delete.js
+++ src/js/api/post/delete.js
@@ -1,9 +1,20 @@
 import { headers } from "../headers";
 import { API_SOCIAL_POSTS } from "../constants";
 
+/**
+ * Deletes a social media post by sending a 'DELETE' request to the 'API_SOCIAL_POST/{id}' endpoint. 
+ * Requires the post's unique 'id' to delete it. 
+ * If the deletion is successful, it alerts the user and redirects them to the homepage.
+ * It throws an error if the deletion fails or the response is not okay.
+ * 
+ * @async
+ * @param {number} id The unique id for the social media post to be deleted.
+ * @returns {void} Alerts the user that the post is deleted successfully, and redirects to the homepage.
+ * @throws {Error} Throws error if the deletion fails.
+ */
+
 export async function deletePost(id) {
-    const postId = new URLSearchParams(window.location.search).get('id');
-    const apiUrl = `${API_SOCIAL_POSTS}/${postId}`;
+    const apiUrl = `${API_SOCIAL_POSTS}/${id}`;
     
     try {
         const requestHeaders = await headers();
@@ -13,7 +24,7 @@ export async function deletePost(id) {
         });
         
         if (response.status === 204) {
-            alert('Blog post deleted successfully.');
+            alert('Post deleted successfully.');
             window.location.href = `/index.html`;
             return;
         } else {
