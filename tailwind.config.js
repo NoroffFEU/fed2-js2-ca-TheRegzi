781a1cd2c20b763a898696302d9ed7e8476404c2
TheRegzi <reginekornbakk@gmail.com>
2024-10-31 16:33:27 +0100

    configured tailwind correctly

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: ["./**/*.{html,js,ts}","!./node_modules/**/*"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
