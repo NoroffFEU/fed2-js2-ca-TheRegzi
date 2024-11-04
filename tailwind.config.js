23402fe6259af0cccc4d6e0e8a64c5b3141c860b
TheRegzi <reginekornbakk@gmail.com>
2024-11-04 13:40:00 +0100

    feat: added styling for header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,31 @@
 module.exports = {
-  content: ["./**/*.{html,js,ts}","!./node_modules/**/*"],
+  content: ["./**/*.{html,js,ts}", "!./node_modules/**/*"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#EBDBFA", 
+        secondary: "#F3E9FD", 
+        accent: "#655469", 
+        customRed: "#B40003",
+      },
+      fontFamily: {
+        heading: ["Raleway", "sans-serif"],
+        post: ["Source sans 3", "sans-serif"],
+        accent: ["Poppins", "sans-serif"]
+      },
+      fontSize: {
+	      xs: "0.7rem",
+	      sm: "1rem", 
+	      lg: "1.3rem", 
+	      xl: "2rem", 
+	    },
+      screens: {
+        sm: '640px',  
+        md: '754px',  
+        lg: '1024px', 
+        xl: '1280px',
+      },
+    },
   },
   plugins: [],
-}
+};
